e2926238f8b27eff755a7ce080055cbe61a3fa24
Chen Bin <chenbin@uniontech.com>
2021-04-26 14:33:26 +0800

    chore: 单元测试文件增加copyright
    补上单元测试文件遗漏的copyright
    
    Log:
    Change-Id: I57ef8f8156408808ec2e8b9368280b2348725585

--- tests/src/ut_dguiapplicationhelper.cpp
+++ tests/src/ut_dguiapplicationhelper.cpp
@@ -1,8 +1,30 @@
-#include <gtest/gtest.h>
+/*
+ * Copyright (C) 2021 ~ 2021 Deepin Technology Co., Ltd.
+ *
+ * Author:     Chen Bin <chenbin@uniontech.com>
+ *
+ * Maintainer: Chen Bin <chenbin@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QMap>
+
+#include "test.h"
 #include "dguiapplicationhelper.h"
 #include "dguiapplicationhelper_p.h"
 
-#include <QMap>
 
 DGUI_BEGIN_NAMESPACE
 
