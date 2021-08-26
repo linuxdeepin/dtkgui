ba323650feb31ded1fdfa4bd40d75aab80e8dbe5
Wang Penga <wangpenga@uniontech.com>
2021-08-26 17:37:21 +0800

    fix: 排除无法测试的源文件
    单元测试覆盖率生成排除无法测试的源文件
    
    Log: 单元测试覆盖范围调整
    Change-Id: I8b05021e7c12e3779236cc2b64429bb9f3eeb89b

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -21,7 +21,7 @@ TESTARGS="--gtest_output=xml:dde_test_report_dtkgui.xml"  make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 #lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
-lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build/src*" --output-file coverage.info
+lcov --remove coverage_all.info "*/filedrag/*" "*/dbus/*" "*/tests/*" "*/usr/include*" "*build/src*" --output-file coverage.info
 cd ..
 genhtml -o $REPORT_DIR $BUILD_DIR/coverage.info
 
