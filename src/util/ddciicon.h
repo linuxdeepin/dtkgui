2b30030af20c770d3f364cfaa6ffa890616d8202
zccrs <zccrs@live.com>
2021-09-01 17:40:10 +0800

    feat: 根据DCI标准添加DDciIcon类
     DDciIcon 类为 Dci 文件和 Dci 图标资源提供了一个
    严格标准的图标实例。通过将标准的 DCI 图标文件或图标
    数据解析成可添加到 Dtk 控件中的图标资源。通常的这类
    标准的图标资源都包含{ 深浅主题、图标状态、以及不同类
    型 }。该类将根据当前需要的状态，将对应的图标展现出来。
    
     DDciIcon 所需要的原始数据文件最少需要一个 Normal
    状态下的图标资源，否则此 Icon 将被视为是空图标。对于
    有不同状态的图标资源，DDciIcon 在绘制时会优选选择使
    用其自带的内容，但对于目标状态和图标状态不同的图标资源，
    其绘制会根据画笔的前景色进行统一处理。
    
    针对图标种类，DDciIcon 存储一个当前图标的变量，如果在
    绘制时未指定目标图标种类，则会获取默认的当前图标种类变量
    进行绘制，默认的当前图标种类为 TextType，不同的图标种
    类其绘制方式也不一样，种类包含{ TextType、ActionType、
    IconType }。详细描述请查阅DTK相关文档。
    
    Log:
    Influence: 无
    Change-Id: Ia85d64e0885674d8b8ff8c17587f3781ac54d6b2
    (cherry picked from commit 62f7b1d290841372a6dbe6e03a034bec093febb6)

---
+++ src/util/ddciicon.h
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2021 UnionTech Technology Co., Ltd.
+ *
+ * Author:     zccrs <zccrs@live.com>
+ *
+ * Maintainer: zccrs <zhangjide@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <dtkgui_global.h>
+#include <functional>
+
+#include <QBrush>
+
+DCORE_BEGIN_NAMESPACE
+class DDciFile;
+DCORE_END_NAMESPACE
+
+DGUI_BEGIN_NAMESPACE
+
+class DDciIconPrivate;
+class DDciIcon
+{
+public:
+    enum Type {
+        TextType = 0,
+        ActionType = 1,
+        IconType = 2,
+        CurrentType = 3,
+
+        TypeCount
+    };
+    enum Mode {
+        Normal = 0,
+        Disabled = 1,
+        Hover = 2,
+        Pressed = 3
+    };
+    enum Theme {
+        Light = 0,
+        Dark = 1
+    };
+    enum MatchOption {
+        NoMatchOption = 0,
+        ModeSensitive = 1 << 0,
+        FormatSensitive = 1 << 1,
+        AllMatchOptions = ModeSensitive | FormatSensitive
+    };
+    Q_DECLARE_FLAGS(MatchOptions, MatchOption)
+
+    DDciIcon(Type ct = DDciIcon::TextType);
+    explicit DDciIcon(const DCORE_NAMESPACE::DDciFile *dciFile, DDciIcon::Type ct = DDciIcon::TextType);
+    explicit DDciIcon(const QString &fileName, DDciIcon::Type ct = DDciIcon::TextType);
+    explicit DDciIcon(const QByteArray &data, DDciIcon::Type ct = DDciIcon::TextType);
+    DDciIcon(const DDciIcon &other);
+    DDciIcon &operator=(const DDciIcon &other) noexcept;
+    ~DDciIcon();
+    DDciIcon(DDciIcon && other) noexcept;
+    DDciIcon &operator=(DDciIcon &&other) noexcept;
+    void swap(DDciIcon &other) noexcept { d.swap(other.d); }
+
+    void setCurrentType(Type type);
+    Type currentType() const;
+
+    bool isNull(DDciIcon::Type type = CurrentType) const;
+
+    struct Icon : public QSharedData {
+        int iconSize;
+        Type type;
+        Mode mode;
+        Theme theme;
+        QByteArray format;
+
+        struct Data {
+            int imagePixelRatio;
+            QByteArray foreground;
+            QByteArray background;
+        };
+        QVector<Data> datas;
+    };
+    using IconPointer = QSharedDataPointer<Icon>;
+
+    using IconMatcher = std::function<bool(const IconPointer&)>;
+    IconPointer findIcon(IconMatcher matcher) const;
+    IconPointer findIcon(int iconSize, Theme theme, Mode mode = Normal, Type type = CurrentType,
+                         const QByteArray &requestFormat = QByteArray(),
+                         MatchOptions options = NoMatchOption) const;
+
+    static QPixmap generatePixmap(const IconPointer &icon, Mode mode, int iconSize,
+                                  qreal devicePixelRatio, const QBrush &foreground = QBrush());
+    static void paint(const IconPointer &icon, Mode mode, int iconSize, qreal devicePixelRatio,
+                      QPainter *painter, const QRect &rect, Qt::Alignment alignment = Qt::AlignCenter);
+    // TODO: Should be compatible with QIcon
+private:
+    QSharedDataPointer<DDciIconPrivate> d;
+    DDciIcon::Type currentIconType;
+};
+
+Q_DECLARE_INCOMPATIBLE_FLAGS(DDciIcon::MatchOptions)
+
+DGUI_END_NAMESPACE
