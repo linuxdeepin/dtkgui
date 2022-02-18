533d213b1b9584f48ca7000afc68c7793a8fa5f2
Chen Bin <chenbin@uniontech.com>
2022-02-18 16:16:17 +0800

    feat: Add dci image convert tool
    Some images saved into dci icon should convert it's format
    to alpha8. therefore, we need to add a tool to complete the
    format conversion.
    
    Log:
    Influence: none
    Change-Id: Ia9e146db60e9e7cc4b323c5a4b892777a9313a42

---
+++ tools/dci-image-converter/dci-image-converter.pro
@@ -0,0 +1,6 @@
+CONFIG += c++11 console
+CONFIG -= app_bundle
+
+SOURCES += \
+    $$PWD/main.cpp \
+
---
+++ tools/dci-image-converter/main.cpp
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2022 UnionTech Technology Co., Ltd.
+ *
+ * Author:     Chen Bin <chenbin@uniontech.com>
+ *
+ * Maintainer: Chen Bin <chenbin@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QCommandLineParser>
+#include <QCoreApplication>
+#include <QDir>
+#include <QFileInfo>
+#include <QImage>
+
+#define ALPHA8STRING QLatin1String("alpha8")
+
+static bool convertImageTo(const QString &originPath, const QString &targetPath)
+{
+    QImage originImage(originPath);
+    if (originImage.isNull())
+        return false;
+    const auto fi = QFileInfo(originPath);
+    const QString &suffix = fi.suffix();
+
+    // ###(Chen bin): The tool only determines whether the image is suitable
+    // for conversion，and does not judge the palette.
+    if ((originImage.width() % 2) || (originImage.height() % 2))
+        return false;
+
+    auto tmp = originImage.convertToFormat(QImage::Format_Alpha8);
+    QImage alpha(tmp.constBits(), tmp.width(), tmp.height(), tmp.bytesPerLine(), QImage::Format_Grayscale8);
+    return alpha.save(targetPath, suffix.toLocal8Bit());
+}
+
+static bool convertImageFrom(const QString &originPath, const QString &targetPath) {
+    QString path(originPath);
+    auto fi = QFileInfo(originPath);
+    QString suffix = fi.suffix();
+    if (suffix.compare(ALPHA8STRING, Qt::CaseInsensitive))
+        return false;  // No convert to alpha8.
+    path.remove(QLatin1Char('.') + fi.suffix());
+    QImage originImage(originPath);
+    if (originPath.isNull() || originImage.format() != QImage::Format_Grayscale8)
+        return false;
+    suffix = QFileInfo(path).suffix();
+    QImage targetImage(originImage.bits(), originImage.width(), originImage.height(),
+                       originImage.bytesPerLine(), QImage::Format_Alpha8);
+    return targetImage.save(targetPath, suffix.toLocal8Bit());
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    a.setApplicationName("dci-image-converter");
+    a.setApplicationVersion("0.0.1");
+
+    QCommandLineParser commandParser;
+
+    auto options = QList<QCommandLineOption> {
+        QCommandLineOption("toAlpha8", "Convert image format to alpha8.", "targetPath"),
+        QCommandLineOption("fromAlpha8", "Convert image format from alpha8.", "targetPath"),
+    };
+
+    commandParser.addOptions(options);
+    commandParser.addPositionalArgument("sourcesPath", "The file path of the original image to be converted.",
+                                        "[dir1 dir2...]/[file1 file2...]");
+    commandParser.addHelpOption();
+    commandParser.addVersionOption();
+    commandParser.process(a);
+
+    auto arguments = commandParser.positionalArguments();
+    if (arguments.isEmpty())
+        commandParser.showHelp(-1);
+    if (commandParser.isSet(options.at(0))) {
+        if (!convertImageTo(arguments.first(), commandParser.value(options.at(0)))) {
+            printf("Convert image failed.\n");
+            return 1;
+        }
+    } else if (commandParser.isSet(options.at(1))) {
+        if (!convertImageFrom(arguments.first(), commandParser.value(options.at(1)))) {
+            printf("Convert image failed.\n");
+            return 1;
+        }
+    } else {
+        commandParser.showHelp(-1);
+    }
+
+    return 0;
+}
