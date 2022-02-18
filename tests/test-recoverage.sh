55351686f0957f0fb5e3c4fd6c462b0ac6612245
AlexOne <wangpenga@uniontech.com>
2022-02-18 17:35:20 +0800

    chore: 按照要求修改单元测试生成文件名
    按照要求修改单元测试生成文件名
    
    Log:
    Influence: none
    Change-Id: I032d0f01fc9e11238b6e7a81bf08d62eb81074e0

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -17,13 +17,13 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 qmake .. CONFIG+=debug
 export ASAN_OPTIONS=halt_on_error=0
-TESTARGS="--gtest_output=xml:${XML_DIR}/dde_test_report_dtkgui.xml"  make check -j$(nproc)
+TESTARGS="--gtest_output=xml:${XML_DIR}/report_dtkgui.xml"  make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 #lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
 lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build-ut/src*" --output-file coverage.info
 cd ..
-genhtml -o $HTML_DIR $BUILD_DIR/coverage.info
+genhtml -o $HTML_DIR $BUILD_DIR/coverage.info && mv ${BUILD_DIR}/html/index.html ${BUILD_DIR}/html/cov_dtkgui.html
 
 test -e ${BUILD_DIR}/asan.log* && mv ${BUILD_DIR}/asan.log* ${BUILD_DIR}/asan_dtkgui.log || touch ${BUILD_DIR}/asan_dtkgui.log
 
