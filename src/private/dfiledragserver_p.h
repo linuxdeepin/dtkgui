987b356f46f887f85e2bdae055e0a83c6931ec6f
xzl <1037981455@qq.com>
2022-08-11 11:20:02 +0800

    chore: adapt REUSE license header
    修改版权声明
    
    Log: 修改版权声明

--- src/private/dfiledragserver_p.h
+++ src/private/dfiledragserver_p.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #ifndef DFILEDRAGSERVER_P_H
 #define DFILEDRAGSERVER_P_H
 
