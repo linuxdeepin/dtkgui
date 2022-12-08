9dc1bb90d120d60a21009713f1c9eb482a1236d9
renbin <renbin@uniontech.com>
2022-12-08 15:21:15 +0800

    feat: Add image algorithms, ut and comment.
    Add image algorithms, unit test cases, dox file comment and example.
    
    Log: Add image algorithms, ut and comment.

--- include/util/dimagehandler.h
+++ include/util/dimagehandler.h
@@ -32,17 +32,42 @@ public:
     void clearCache();
 
     bool saveImage(const QString &fileName, const QString &format = QString());
+    bool saveImage(const QImage &image, const QString &fileName, const QString &format = QString());
     bool rotateImage(QImage &image, int angle);
     bool rotateImageFile(const QString &fileName, int angle);
 
     bool isReadable() const;
     bool isWriteable() const;
-    bool isRotateable() const;
+    bool isRotatable() const;
 
     QString lastError() const;
 
     static QStringList supportFormats();
     static QString detectImageFormat(const QString &fileName);
+
+    static QImage oldColorFilter(const QImage &img);
+    static QImage warmColorFilter(const QImage &img, int intensity = 30);
+    static QImage coolColorFilter(const QImage &img, int intensity = 30);
+    static QImage grayScaleColorFilter(const QImage &img);
+    static QImage antiColorFilter(const QImage &img);
+    static QImage metalColorFilter(const QImage &img);
+
+    static QImage bilateralFilter(const QImage &img, double spatialDecay, double photometricStandardDeviation);
+    static QImage contourExtraction(const QImage &img);
+    static QImage binaryzation(const QImage &img);
+    static QImage grayScale(const QImage &img);
+
+    static QImage laplaceSharpen(const QImage &img);
+    static QImage sobelEdgeDetector(const QImage &img);
+
+    static QImage changeLightAndContrast(const QImage &img, int light = 100, int contrast = 150);
+    static QImage changeBrightness(const QImage &img, int brightness);
+    static QImage changeTransparency(const QImage &img, int transparency);
+    static QImage changeStauration(const QImage &img, int saturation);
+    static QImage replacePointColor(const QImage &img, QColor oldColor, QColor newColor);
+
+    static QImage flipHorizontal(const QImage &img);
+    static QImage flipVertical(const QImage &img);
 
 private:
     D_DECLARE_PRIVATE(DImageHandler)
