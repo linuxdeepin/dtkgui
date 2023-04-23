af122a64033f16b7014d7ff5dfa94e43fad45972
YeShanShan <yeshanshan@uniontech.com>
2023-04-23 11:23:34 +0800

    fix: Inconsistent themeType when setThemeType
      When we call setThemeType to DarkType, it will sync DConfig's
    themeType, and reset it to UnknowType, and it cause ut failed.

--- src/kernel/dguiapplicationhelper.cpp
+++ src/kernel/dguiapplicationhelper.cpp
@@ -1700,6 +1700,7 @@ void DGuiApplicationHelper::setPaletteType(DGuiApplicationHelper::ColorType pale
 {
     D_D(DGuiApplicationHelper);
 
+    d->initPaletteType();
     d->setPaletteType(paletteType, true);
 }
 
