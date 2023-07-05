a740797a4b82868d972675e90453cebfd9ae6292
wangfei <wangfei@deepin.org>
2023-07-05 11:37:24 +0800

    chore: adjust xdgiconproxyengine
    add difference about xdgiconproxyengine from qt5integration
    
    Log: add difference about xdgiconproxyengine from qt5integration

--- src/util/private/xdgiconproxyengine.cpp
+++ src/util/private/xdgiconproxyengine.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2022 - 2023 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
@@ -16,32 +16,36 @@
 #include <cxxabi.h>
 #include <qmath.h>
 
+static inline bool XdgIconFollowColorScheme()
+{
+    return XdgIconLoader::instance()->followColorScheme();
+}
+
+static inline QPixmap entryPixmap(ScalableEntry *color_entry, const QSize &size, QIcon::Mode mode, QIcon::State state)
+{
+    if (!color_entry)
+        return QPixmap();
+
+    if (auto d = color_entry->svgIcon.data_ptr())
+        if (d->engine)
+            return d->engine->pixmap(size, mode, state);
+
+    return color_entry->svgIcon.pixmap(size, mode, state);
+}
+
+static const QString STYLE = QStringLiteral(".ColorScheme-Text, .ColorScheme-NeutralText{color:%1;}\
+\n.ColorScheme-Highlight{color:%2;}");
+
 #if XDG_ICON_VERSION_MAR >= 3
 #define private public
 #include <private/xdgiconloader/xdgiconloader_p.h>
 #undef private
-#elif XDG_ICON_VERSION_MAR == 2
-//这个版本中的xdgiconloader_p.h定义和qiconloader_p.h有冲突
-//只能通过此方式提供创建XdgIconLoaderEngine对象的接口
-#include "xdgiconenginecreator.h"
-#endif
 
-static bool XdgIconFollowColorScheme()
-{
-    // XdgIcon::followColorScheme()
-    return XdgIconLoader::instance()->followColorScheme();
+namespace DEEPIN_XDG_THEME {
+static QThreadStorage<PALETTE_MAP> colorScheme; // <type, color>
 }
 
-#if XDG_ICON_VERSION_MAR >= 3
-namespace DEEPIN_QT_THEME {
-QThreadStorage<QString> colorScheme;
-void (*setFollowColorScheme)(bool);
-bool (*followColorScheme)();
-} // namespace DEEPIN_QT_THEME
-#endif
-
 DGUI_BEGIN_NAMESPACE
-
 XdgIconProxyEngine::XdgIconProxyEngine(XdgIconLoaderEngine *proxy)
     : engine(proxy)
     , lastMode(QIcon::Normal)
@@ -59,6 +63,7 @@ quint64 XdgIconProxyEngine::entryCacheKey(const ScalableEntry *color_entry, cons
     return quint64(color_entry) ^ (quint64(mode) << 56) ^ (quint64(state) << 48);
 }
 
+
 QPixmap XdgIconProxyEngine::followColorPixmap(ScalableEntry *color_entry, const QSize &size, QIcon::Mode mode, QIcon::State state)
 {
     if (mode == QIcon::Selected && mode != lastMode) {
@@ -72,14 +77,14 @@ QPixmap XdgIconProxyEngine::followColorPixmap(ScalableEntry *color_entry, const 
 
     lastMode = mode;
     quint64 cache_key = entryCacheKey(color_entry, mode, state);
-    const QString &cache_color_scheme = entryToColorScheme.value(cache_key);
+    const DEEPIN_XDG_THEME::PALETTE_MAP &cache_color_scheme = entryToColorScheme.value(cache_key);
 
     // 当size为1时表示此svg文件不需要处理ColorScheme标签
-    if (cache_color_scheme.size() == 1)
+    if (!cache_color_scheme.isEmpty() && cache_color_scheme[DEEPIN_XDG_THEME::Text].size() == 1)
         return color_entry->pixmap(size, mode, state);
 
-    const QString &color_scheme = DEEPIN_QT_THEME::colorScheme.localData();
-    QPixmap pm = color_scheme == cache_color_scheme ? color_entry->svgIcon.pixmap(size, mode, state) : QPixmap();
+    const DEEPIN_XDG_THEME::PALETTE_MAP &color_scheme = DEEPIN_XDG_THEME::colorScheme.localData();
+    QPixmap pm = color_scheme == cache_color_scheme ? entryPixmap(color_entry, size, mode, state) : QPixmap();
     // Note: not checking the QIcon::isNull(), because in Qt5.10 the isNull() is not reliable
     // for svg icons desierialized from stream (see https://codereview.qt-project.org/#/c/216086/)
     if (pm.isNull()) {
@@ -92,7 +97,7 @@ QPixmap XdgIconProxyEngine::followColorPixmap(ScalableEntry *color_entry, const 
         if (device.open(QIODevice::ReadOnly)) {
             // Note: indexes are assembled as in qtsvg (QSvgIconEnginePrivate::hashKey())
             QPair<int, QString> style_sheet;
-            style_sheet = qMakePair((mode << 4) | state, QStringLiteral(".ColorScheme-Text{color:%1;}").arg(color_scheme));
+            style_sheet = qMakePair((mode << 4) | state, STYLE.arg(color_scheme[DEEPIN_XDG_THEME::Text], color_scheme[DEEPIN_XDG_THEME::Highlight]));
             QSharedPointer<QXmlStreamWriter> writer(new QXmlStreamWriter {&svg_buffers[style_sheet.first]});
 
             QXmlStreamReader xmlReader(&device);
@@ -120,7 +125,7 @@ QPixmap XdgIconProxyEngine::followColorPixmap(ScalableEntry *color_entry, const 
 
         if (invalidBuffers) {
             // 此svg图标无ColorScheme标签时不应该再下面的操作，且应该记录下来，避免后续再处理svg文件内容
-            entryToColorScheme[cache_key] = QStringLiteral("#");
+            entryToColorScheme[cache_key] = DEEPIN_XDG_THEME::PALETTE_MAP({ {DEEPIN_XDG_THEME::Text, "#"} });
             return color_entry->pixmap(size, mode, state);
         }
 
@@ -134,19 +139,20 @@ QPixmap XdgIconProxyEngine::followColorPixmap(ScalableEntry *color_entry, const 
         str.setVersion(QDataStream::Qt_4_4);
         QHash<int, QString> filenames;
         filenames[0] = color_entry->filename; // Note: filenames are ignored in the QSvgIconEngine::read()
-        filenames[-1] = color_scheme; // 在dsvg插件中会为svg图标做缓存，此处是为其添加额外的缓存文件key标识，避免不同color的svg图标会命中同一个缓存文件
+        filenames[-1] = color_scheme[DEEPIN_XDG_THEME::Text] + color_scheme[DEEPIN_XDG_THEME::Highlight]; // 在dsvg插件中会为svg图标做缓存，此处是为其添加额外的缓存文件key标识，避免不同color的svg图标会命中同一个缓存文件
         str << QStringLiteral("svg") << filenames << static_cast<int>(0) /*isCompressed*/ << svg_buffers << static_cast<int>(0) /*hasAddedPimaps*/;
 
         QDataStream str_read {&icon_arr, QIODevice::ReadOnly};
         str_read.setVersion(QDataStream::Qt_4_4);
 
         str_read >> color_entry->svgIcon;
-        pm = color_entry->svgIcon.pixmap(size, mode, state);
+
+        pm = entryPixmap(color_entry, size, mode, state);
 
         // load the icon directly from file, if still null
         if (pm.isNull()) {
             color_entry->svgIcon = QIcon(color_entry->filename);
-            pm = color_entry->svgIcon.pixmap(size, mode, state);
+            pm = entryPixmap(color_entry, size, mode, state);
         }
 
         entryToColorScheme[cache_key] = color_scheme;
@@ -158,7 +164,7 @@ QPixmap XdgIconProxyEngine::followColorPixmap(ScalableEntry *color_entry, const 
 QPixmap XdgIconProxyEngine::pixmapByEntry(QIconLoaderEngineEntry *entry, const QSize &size, QIcon::Mode mode, QIcon::State state)
 {
     if (!XdgIconFollowColorScheme()) {
-        DEEPIN_QT_THEME::colorScheme.setLocalData(QString());
+        DEEPIN_XDG_THEME::colorScheme.setLocalData(DEEPIN_XDG_THEME::PALETTE_MAP());
 
         return entry->pixmap(size, mode, state);
     }
@@ -167,9 +173,12 @@ QPixmap XdgIconProxyEngine::pixmapByEntry(QIconLoaderEngineEntry *entry, const Q
     char *type_name = abi::__cxa_demangle(typeid(*entry).name(), 0, 0, 0);
 
     if (type_name == QByteArrayLiteral("ScalableFollowsColorEntry")) {
-        if (DEEPIN_QT_THEME::colorScheme.localData().isEmpty()) {
+        if (DEEPIN_XDG_THEME::colorScheme.localData().isEmpty()) {
             const QPalette &pal = qApp->palette();
-            DEEPIN_QT_THEME::colorScheme.setLocalData(mode == QIcon::Selected ? pal.highlightedText().color().name() : pal.windowText().color().name());
+            DEEPIN_XDG_THEME::colorScheme.setLocalData(DEEPIN_XDG_THEME::PALETTE_MAP({
+                { DEEPIN_XDG_THEME::Text, mode == QIcon::Selected ? pal.highlightedText().color().name() : pal.windowText().color().name() },
+                { DEEPIN_XDG_THEME::Highlight, pal.highlight().color().name() }
+            }));
         }
 
         pixmap = followColorPixmap(static_cast<ScalableEntry *>(entry), size, mode, state);
@@ -178,7 +187,7 @@ QPixmap XdgIconProxyEngine::pixmapByEntry(QIconLoaderEngineEntry *entry, const Q
     }
 
     free(type_name);
-    DEEPIN_QT_THEME::colorScheme.setLocalData(QString());
+    DEEPIN_XDG_THEME::colorScheme.setLocalData(DEEPIN_XDG_THEME::PALETTE_MAP());
 
     return pixmap;
 }
@@ -187,16 +196,24 @@ void XdgIconProxyEngine::paint(QPainter *painter, const QRect &rect, QIcon::Mode
 {
     if (painter->device()->devType() == QInternal::Widget
         && XdgIconFollowColorScheme()
-        && DEEPIN_QT_THEME::colorScheme.localData().isEmpty()) {
+        && DEEPIN_XDG_THEME::colorScheme.localData().isEmpty()) {
         const QPalette &pal = qvariant_cast<QPalette>(dynamic_cast<QObject *>(painter->device())->property("palette"));
-        DEEPIN_QT_THEME::colorScheme.setLocalData(mode == QIcon::Selected ? pal.highlightedText().color().name() : pal.windowText().color().name());
+        DEEPIN_XDG_THEME::colorScheme.setLocalData(DEEPIN_XDG_THEME::PALETTE_MAP({
+            { DEEPIN_XDG_THEME::Text, mode == QIcon::Selected ? pal.highlightedText().color().name() : pal.windowText().color().name() },
+            { DEEPIN_XDG_THEME::Highlight, pal.highlight().color().name() }
+        }));
     }
 
-    const QPixmap pix = pixmap(rect.size(), mode, state);
+    qreal ratio = 1.0;
+    if (qApp->testAttribute(Qt::AA_UseHighDpiPixmaps))
+        ratio = painter->device() ? painter->device()->devicePixelRatioF() : qApp->devicePixelRatio();
+
+    QPixmap pix = pixmap(rect.size() * ratio, mode, state);
 
     if (pix.isNull())
         return;
 
+    pix.setDevicePixelRatio(ratio);
     painter->drawPixmap(rect, pix);
 }
 
@@ -207,7 +224,7 @@ QPixmap XdgIconProxyEngine::pixmap(const QSize &size, QIcon::Mode mode, QIcon::S
     QIconLoaderEngineEntry *entry = engine->entryForSize(size);
 
     if (!entry) {
-        DEEPIN_QT_THEME::colorScheme.setLocalData(QString());
+        DEEPIN_XDG_THEME::colorScheme.setLocalData(DEEPIN_XDG_THEME::PALETTE_MAP());
 
         return QPixmap();
     }
@@ -265,7 +282,7 @@ void XdgIconProxyEngine::virtual_hook(int id, void *data)
         qGuiApp->setAttribute(Qt::AA_UseHighDpiPixmaps, false);
         arg.pixmap = entry ? pixmapByEntry(entry, arg.size, arg.mode, arg.state) : QPixmap();
         qGuiApp->setAttribute(Qt::AA_UseHighDpiPixmaps, useHighDpiPixmap);
-        DEEPIN_QT_THEME::colorScheme.setLocalData(QString());
+        DEEPIN_XDG_THEME::colorScheme.setLocalData(DEEPIN_XDG_THEME::PALETTE_MAP());
 
         return;
     }
@@ -275,3 +292,4 @@ void XdgIconProxyEngine::virtual_hook(int id, void *data)
 }
 
 DGUI_END_NAMESPACE
+#endif
