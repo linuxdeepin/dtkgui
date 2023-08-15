f08f6ea5385e775bdd79428bc1d6beae0b031f66
ck <chenke@deepin.org>
2023-08-15 14:40:09 +0800

    chore: ut build without source
    - filedrag destroy server to emit serverDestroyed
    - compare color sometime failed, convert to rgb first
    - ut build without source(cmake -DEnableCov=ON)

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -4,6 +4,8 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
+set -ex
+
 BUILD_DIR=`pwd`/../build/tests/
 HTML_DIR=${BUILD_DIR}/html
 XML_DIR=${BUILD_DIR}/report
@@ -13,7 +15,7 @@ export ASAN_OPTIONS="halt_on_error=0"
 # back to project directroy
 cd ..
 
-cmake -Bbuild -DCMAKE_BUILD_TYPE=Debug 
+cmake -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_EXAMPLE=OFF -DBUILD_DOCS=OFF -DEnableCov=ON 
 
 cmake --build build --target ut-DtkGui -j$(nproc)
 
@@ -21,13 +23,9 @@ cd $BUILD_DIR
 
 ./ut-DtkGui --gtest_output=xml:${XML_DIR}/report_dtkgui.xml
 
-lcov -d ./ -c -o coverage_all.info
-#lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
+lcov -d ../ -c -o coverage_all.info
 lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build-ut/src*" --output-file coverage.info
 cd ..
 genhtml -o $HTML_DIR $BUILD_DIR/coverage.info && mv ${BUILD_DIR}/html/index.html ${BUILD_DIR}/html/cov_dtkgui.html
 
 test -e ${BUILD_DIR}/asan.log* && mv ${BUILD_DIR}/asan.log* ${BUILD_DIR}/asan_dtkgui.log || touch ${BUILD_DIR}/asan_dtkgui.log
-
-#rm -rf $BUILD_DIR
-#rm -rf ../$BUILD_DIR
