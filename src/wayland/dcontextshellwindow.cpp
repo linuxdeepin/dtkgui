4fb86c1204df63acd4d8530ffaf38a848b54d404
Zhang Dingyuan <zhangdingyuan@deepin.org>
2024-08-27 13:45:45 +0800

    chore: update license info
    as title
    
    Log:

--- src/wayland/dcontextshellwindow.cpp
+++ src/wayland/dcontextshellwindow.cpp
@@ -1,6 +1,7 @@
-// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
 #include "dcontextshellwindow.h"
 
 #include "qwaylandpersonalizationshellintegration_p.h"
