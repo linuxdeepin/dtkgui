49add8b8793191b1951f94cc53dbde879af27fe6
yeshanshan <yeshanshan@uniontech.com>
2025-10-17 14:33:40 +0800

    fix: 没有匹配最佳尺寸的图标
    3b5ab7a: fix: 没有匹配最佳尺寸的图标
    
    原因：存在同名不同尺寸svg图标时，Scalable类型的图标会使用maxSize和minSize去找合适的图标
    ，找到的不一定是最佳尺寸，绘图的时候会进行缩放，导致图标线条变粗/变细
    解决方案：在使用entryForSize查找图标之前，先找一下是否有尺寸完全匹配的图标
    
    Log:
    Influence: dtk内建图标（所有使用dtk内建图标的都会有影响）
    Change-Id: I158283516b067cc0a81654dee8c6657f54b9a56f

--- src/util/private/dbuiltiniconengine.cpp
+++ src/util/private/dbuiltiniconengine.cpp
@@ -248,6 +248,18 @@ QPixmap DBuiltinIconEngine::pixmap(const QSize &size, QIcon::Mode mode,
     return QPixmap();
 }
 
+static QIconLoaderEngineEntry *perfectEntryForSize(const QThemeIconInfo &info, const QSize &size, qreal scale = 1)
+{
+    const int numEntries = info.entries.size();
+    const int iconsize = qMin(size.width(), size.height());
+    for (int i = 0; i < numEntries; ++i) {
+        QIconLoaderEngineEntry *entry = info.entries.at(i);
+        if (static_cast<int>(entry->dir.size * scale) == iconsize)
+            return entry;
+    }
+    return nullptr;
+}
+
 void DBuiltinIconEngine::paint(QPainter *painter, const QRect &rect,
                                QIcon::Mode mode, QIcon::State state)
 {
@@ -256,9 +268,13 @@ void DBuiltinIconEngine::paint(QPainter *painter, const QRect &rect,
     const qreal scale = devicePixelRatio(painter);
     QSize pixmapSize = rect.size() * scale;
 
-    QIconLoaderEngineEntry *entry = QIconLoaderEngine::entryForSize(m_info, pixmapSize);
-    if (!entry)
-        return;
+    // Search perfect entry first
+    QIconLoaderEngineEntry *entry = perfectEntryForSize(m_info, pixmapSize, scale);
+    if (!entry) {
+         entry = QIconLoaderEngine::entryForSize(m_info, pixmapSize);
+         if (!entry)
+             return;
+    }
 
     // 如果有 background 则绘制背景图先
     QString bgFileName = entry->filename + QStringLiteral(".background");
